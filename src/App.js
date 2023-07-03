34a11d9c96ef7193982724f44224818e57e2b164
Abram2928 <muhammadabiyyuramadhan01@gmail.com>
2023-07-03 21:38:46 +0700

    Baru selesai nih

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const nilai = 5
+  let hasil;
+  if (nilai < 10) {
+    hasil = "jelek"
+  } else {
+    hasil = "bagus"
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React = aboiiiii
+        </a>
+        <h1>{hasil}</h1>
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
